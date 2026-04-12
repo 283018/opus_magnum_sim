0bd5631b2a01c756458577363e36e5afce1c3a63
Dolomirr <dolomirr@gmail.com>
2026-04-12 17:38:56 +0200

    simplified index manager to module level

--- src/opus_magnum_sym/elements/env/index_manager.py
+++ src/opus_magnum_sym/elements/env/index_manager.py
@@ -1,56 +1,49 @@
-from typing import Self
+# TODO: simple version for now, up to re-write if multithreading is needed
+
+_next_id: int = 0
+_active: set[int] = set()
+_free: set[int] = set()
 
 
-class IndexManager:
-    _instance: Self
+def allocate() -> int:
+    global _next_id  # noqa: PLW0603
+    idx = _free.pop() if _free else _next_id
+    if idx == _next_id:
+        _next_id += 1
+    _active.add(idx)
+    return idx
 
-    def __init__(self) -> None:
-        self._next_id: int = 0
-        self._active: set[int] = set()
-        self._free_list: set[int] = set()
-        self._max_id: int = 0
 
-    @classmethod
-    def get(cls) -> Self:
-        if cls._instnace is None:
-            cls._instnace = cls()
-        return cls._instance
+def free(idx: int) -> None:
+    if idx not in _active:
+        msg = f"Index {idx} is not currently active."
+        raise ValueError(msg)
+    _active.remove(idx)
+    _free.add(idx)
 
-    @classmethod
-    def reset(cls) -> None:
-        cls._instance = cls()  # ty:ignore[invalid-assignment]
 
-    def allocate(self) -> int:
-        if self._free_list:
-            idx = self._free_list.pop()
-        else:
-            idx = self._next_id
-            self._next_id += 1
-            self._max_id = max(self._max_id, idx)
+def is_active(idx: int) -> bool:
+    return idx in _active
 
-        self._active.add(idx)
-        return idx
 
-    def free(self, idx: int) -> bool:
-        if idx in self._active:
-            self._active.remove(idx)
-            self._free_list.add(idx)
-            return True
-        return False
+def is_valid(idx: int) -> bool:
+    return idx >= 0 and idx < _next_id
 
-    def is_active(self, idx: int) -> bool:
-        return idx in self._active
 
-    def is_valid(self, idx: int) -> bool:
-        return idx >= 0 and idx < self._next_id
+def active_count() -> int:
+    return len(_active)
 
-    @property
-    def active_count(self) -> int:
-        return len(self._active)
 
-    @property
-    def total_allocated(self) -> int:
-        return self._next_id
+def total_allocated() -> int:
+    return _next_id
 
-    def get_all_active(self) -> set[int]:
-        return self._active.copy()
+
+def get_all_active() -> set[int]:
+    return _active.copy()
+
+
+def reset() -> None:
+    global _next_id, _active, _free  # noqa: PLW0602, PLW0603
+    _next_id = 0
+    _active.clear()
+    _free.clear()
