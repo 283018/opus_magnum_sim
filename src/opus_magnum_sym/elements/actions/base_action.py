047d20a789a17496de2db48aa2702bcb494028e3
Dolomirr <dolomirr@gmail.com>
2026-04-13 10:58:24 +0200

    ActionAssignmentError

--- src/opus_magnum_sym/elements/actions/base_action.py
+++ src/opus_magnum_sym/elements/actions/base_action.py
@@ -1,1 +1,17 @@
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from opus_magnum_sym.elements.components.base_component import ComponentType
+
+
+class ActionAssignmentError(Exception):
+    def __init__(
+        self,
+        message,
+        component_type: ComponentType,
+        component_id: int,
+    ) -> None:
+        self.message = message
+        super().__init__(self.message)
+        self.component_type = component_type
+        self.component_id = component_id
