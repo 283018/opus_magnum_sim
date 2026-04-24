ca3546d026fef61f205bc03f122e0bd9302c9d17
dolomirr <dolomirr@pop-os.tail14d09a.ts.net>
2026-04-24 15:44:49 +0200

    add proper components init

---
+++ src/opus_magnum_sym/elements/components/__init__.py
@@ -0,0 +1,10 @@
+from .base_component import ComponentType
+from .enter_exit import Enter, Exit
+from .manupulator import Manupulator
+
+__all__ = [
+    "ComponentType",
+    "Enter",
+    "Exit",
+    "Manupulator",
+]
